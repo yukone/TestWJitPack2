eb08030fcbb9464f576303176b4cd2ccf717b1f1
yukone <yukone@gmail.com>
2017-11-22 09:18:38 +0000

    Initial commit

---
+++ testlib/src/main/java/app/gott/testlib/TestLib.java
@@ -0,0 +1,18 @@
+package app.gott.testlib;
+
+/**
+ * Created by utilizador on 22-11-2017.
+ */
+
+public class TestLib {
+
+    public String sRandomVar;
+
+    public TestLib() {
+    }
+
+    public String GetTestLib()
+    {
+        return "Test";
+    }
+}
